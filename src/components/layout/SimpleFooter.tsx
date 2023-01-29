03cf9f30826b02487fa6249bd1e8bc9cf38aa374
gavsidhu <gavinjeet.sidhu@gmail.com>
2023-01-28 21:00:43 -0800

    chore: added footer component options

---
+++ src/components/layout/SimpleFooter.tsx
@@ -0,0 +1,13 @@
+export default function SimpleFooter() {
+  return (
+    <footer>
+      <div className='mx-auto max-w-7xl px-6 pb-12 pt-32'>
+        <div>
+          <p className='text-center text-xs leading-5 text-gray-500'>
+            &copy; 2023 Uplevel HQ LLC. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
