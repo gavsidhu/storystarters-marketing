e97c8658962f00c9025a1388e6553a3f6d92486a
gavsidhu <gavinjeet.sidhu@gmail.com>
2023-02-24 13:40:40 -0800

    feat: added pricing page - not finished

---
+++ src/pages/pricing.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+// const pricingDetails = {
+//   tiers: [
+//     {
+//       title: 'Free',
+//       price: 0,
+//       frequency: '/month',
+//       description: 'Try Story Starters for free',
+//       features: [
+//         '5,000 words',
+//         'Plot generator',
+//         'Character creator',
+//         'Story outline generator',
+//       ],
+//       cta: 'Start For Free',
+//       mostPopular: false,
+//     },
+//     {
+//       title: 'Starter',
+//       price: 15,
+//       frequency: '/month',
+//       description: 'Ideal for aspiring writers or those just starting out.',
+//       features: [
+//         '20,000 words per month',
+//         'Plot generator',
+//         'Character creator',
+//         'Story outline generator',
+//       ],
+//       cta: 'Get Started',
+//       mostPopular: false,
+//     },
+//     {
+//       title: 'Standard',
+//       price: 60,
+//       frequency: '/month',
+//       description:
+//         'Perfect for writers looking to take their writing to the next level.',
+//       features: [
+//         '100,000 words per month',
+//         'Plot generator',
+//         'Character creator',
+//         'Story outline generator',
+//       ],
+//       cta: 'Get Started',
+//       mostPopular: false,
+//     },
+//     {
+//       title: 'Professional',
+//       price: 100,
+//       frequency: '/month',
+//       description:
+//         'Tailored for professional writers who are looking to elevate their craft.',
+//       features: [
+//         '200,000 words per month',
+//         'Plot generator',
+//         'Character creator',
+//         'Story outline generator',
+//       ],
+//       cta: 'Get Started',
+//       mostPopular: false,
+//     },
+//   ],
+// };
+const pricing = () => {
+  return <div>pricing</div>;
+};
+
+export default pricing;
