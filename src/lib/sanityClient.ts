9c97b0c95d3b10fff532b646d792ba645ec546c6
gavsidhu <gavinjeet.sidhu@gmail.com>
2023-03-08 13:05:41 -0800

    fix: changed to createClient from sanityClient, default export @sanity/client has been deprecated

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,7 +1,7 @@
-import sanityClient from '@sanity/client';
+import { createClient } from '@sanity/client';
 
-export default sanityClient({
-  projectId: 'r8h6cmp9', // you can find this in sanity.json
+export default createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
 });
