cdd89ad70b9436a03ab87180962e2fa30d61108e
gavsidhu <gavinjeet.sidhu@gmail.com>
2023-03-08 13:32:25 -0800

    build: added snaity server and sanity config

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -4,5 +4,5 @@ export default createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data,
-  apiVersion: '2022-03-13',
+  apiVersion: '2021-10-21',
 });
